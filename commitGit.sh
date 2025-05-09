58cb6ebcafd8bc045c623a8c340562885b5aca69
veritas <veritas-k@qq.com>
2025-05-09 15:47:32 +0800

    update commit script

--- commitGit.sh
+++ commitGit.sh
@@ -2,15 +2,17 @@
 
 commit_repo() {
     local repo_name=$1
-    cd "src/$repo_name" || { echo "Repository $repo_name not found!"; return; }
-    echo "Committing changes in $repo_name repository..."
-    echo "Enter commit message, or press Enter to skip:"
+    cd "src/$repo_name" || { echo -e "\033[31mRepository $repo_name not found!\033[0m"; return; }
+    echo -e "\033[34mCommitting changes in $repo_name repository...\033[0m"
+    git status
+    echo -e "\033[33mEnter commit message, or press Enter to skip:\033[0m"
     read message
     if [ -z "$message" ]; then
-        echo "No commit message provided, skipping commit."
+        echo -e "\033[31mNo commit message provided, skipping commit.\033[0m"
     else
         git add .
         git commit -m "$message"
+        echo -e "\033[32mCommit successful!\033[0m"
     fi
     git push
     cd - > /dev/null || return
@@ -21,13 +23,15 @@ commit_repo "plane_part_assemble_task"
 commit_repo "plane_part_moveit_config"
 commit_repo "plane_part_stewart_driver"
 
-echo "Commiting changes in main repository..."
-echo "Enter commit message, or press Enter to skip:"
+echo -e "\033[34mCommiting changes in main repository...\033[0m"
+git status
+echo -e "\033[33mEnter commit message, or press Enter to skip:\033[0m"
 read message
 if [ -z "$message" ]; then
-    echo "No commit message provided, skipping commit."
+    echo -e "\033[31mNo commit message provided, skipping commit.\033[0m"
 else
     git add .
     git commit -m "$message"
     git push
+    echo -e "\033[32mCommit successful!\033[0m"
 fi
